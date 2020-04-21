b2b23d2ea96dfedba5f0da40d5bf42cc1c9a6be6
Lucas Zamboni <lucasbemo@gmail.com>
2020-04-21 17:35:44 -0300

    Including itens into TodoContainer

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,35 @@
 import React from "react"
 
 class TodoContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                tittle: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                tittle: "Develop website and add content",
+                completed: true
+            },
+            {
+                id: 3,
+                tittle: "Deploy to live server",
+                completed: true
+            }
+        ]
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <h1>Hello from Create React App</h1>
-                <p>I'm in React Component!</p>
-            </React.Fragment>
+            <div>
+                {
+                    this.state.todos.map(todo => (
+                        <li>{todo.tittle}</li>
+                    ))
+                }
+            </div>
         )
     }
 }
