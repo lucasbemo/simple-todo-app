febafb11405d97dacd0fbb7172bbaae060353655
Lucas Zamboni <lucasbemo@gmail.com>
2020-04-21 17:25:50 -0300

    Removing all things e creating new things, just TodoContainer Component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello from Create React App</h1>
+                <p>I'm in React Component!</p>
+            </React.Fragment>
+        )
+    }
+}
+export default TodoContainer
