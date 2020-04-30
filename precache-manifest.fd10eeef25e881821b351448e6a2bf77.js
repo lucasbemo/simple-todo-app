d480b2ed0732ba59f2dc14b82705df90232cfe5b
Lucas Zamboni <lucasbemo@gmail.com>
2020-04-30 08:51:07 -0300

    Updates

---
+++ precache-manifest.fd10eeef25e881821b351448e6a2bf77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e299a979f170a10cf0c78df9d76147a",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "d8cccf532d55ea8d817e",
+    "url": "/simple-todo-app/static/css/main.2762fa82.chunk.css"
+  },
+  {
+    "revision": "ed6845579e386b7646f8",
+    "url": "/simple-todo-app/static/js/2.b29d4438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.b29d4438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cccf532d55ea8d817e",
+    "url": "/simple-todo-app/static/js/main.3b1dcf7d.chunk.js"
+  },
+  {
+    "revision": "c167c026fe593c9800d4",
+    "url": "/simple-todo-app/static/js/runtime-main.3fc945e3.js"
+  }
+]);
