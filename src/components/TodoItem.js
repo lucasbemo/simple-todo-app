10e5a0484cc6ba594e197c82e890086ae57d0453
Lucas Zamboni <lucasbemo@gmail.com>
2020-04-30 07:24:58 -0300

    Handling forms

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,27 @@ import React from "react"
 
 class TodoItem extends React.Component {
     render() {
-        return <li>{this.props.todo.tittle}</li>
+      const completedStyle = {
+        fontStyle: "italic",
+        color: "#d35e0f",
+        opacity: 0.4,
+        textDecoration: "line-through"
+      };
+  
+      const { completed, id, title } = this.props.todo;
+  
+      return (
+        <li className="todo-item">
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </li>
+      );
     }
-}
-
-export default TodoItem
+  }
+  
+  export default TodoItem;
